f863f24c152c514e9c1082600bf9091597633c69
MasonM <Mason.P.Marinko@gmail.com>
2022-11-26 00:26:59 -0700

    early commit, laying out project, tailwind added, setup nav bar, body, footer, and other future components

---
+++ components/global/nav/index.tsx
@@ -0,0 +1,11 @@
+
+const NavBar = () => {
+return (
+    <div className="bg-gradient-to-b from-blue-800 to-transparent">
+        <h1 className="h-32">Navigation Bar</h1>
+    </div>
+)
+
+}
+
+export default NavBar
